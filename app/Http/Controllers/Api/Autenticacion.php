a5b0ddf92a4f054297e51a23b4ec93f939c9a252
Piero <108848842+piero-uta@users.noreply.github.com>
2023-09-19 18:25:24 -0300

    api - basico

---
+++ app/Http/Controllers/Api/Autenticacion.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use App\Models\User;
+
+class Autenticacion extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        //obtener los datos del request
+        $credentials = $request->only('email', 'password');
+
+
+
+        
+        if(Auth::attempt($credentials))
+        {
+            $user = User::where('email', $request->email)->first();
+            $token = $user->createToken('authToken')->plainTextToken;
+            return response()->json(['token' => $token], 200);
+        }
+        else
+        {
+            return response()->json(['error' => 'Usuario o contraseña incorrecta'], 401);
+        }
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Autenticacion;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [Autenticacion::class, 'login'])->name('api.login');
+
+Route::group (['middleware' => ['auth:sanctum']], function () {
+    
+});   
