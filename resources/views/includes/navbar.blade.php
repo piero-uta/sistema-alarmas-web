8d025bd9d56f8b1a01da4a335952558c5789b390
Piero <108848842+piero-uta@users.noreply.github.com>
2023-09-19 18:45:31 -0300

    sidebar

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,17 @@
+<div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+<a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+    <span class="fs-4">Sidebar</span>
+</a>
+<hr>
+<ul class="nav nav-pills flex-column mb-auto">
+    <li>
+        <a href="/comunidades" class="nav-link">
+            Comunidades
+        </a>
+        <a href="/usuarios" class="nav-link active">
+            Usuarios
+        </a>
+    </li>
+
+</ul>
+</div>
