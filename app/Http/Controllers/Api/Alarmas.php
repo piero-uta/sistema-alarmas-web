f02cc239d6e97d51a2b69525322c36774762da1f
olverarcedev <olverarcedev@gmail.com>
2023-09-29 18:46:53 -0300

    update alarmas

--- app/Http/Controllers/Api/Alarmas.php
+++ app/Http/Controllers/Api/Alarmas.php
@@ -22,11 +22,11 @@ class Alarmas extends Controller
         $user = $request->user();
         $title = "$user->nombre $user->apellido_paterno $user->apellido_materno";
         $body = "El usuario $user->nombre esta en emergencia";
-        $avatar = isset($user->avatar) ? $user->avatar : 'https://seeklogo.com/images/F/firebase-logo-402F407EE0-seeklogo.com.png';
+        $avatar = isset($user->avatar) ? $user->avatar : 'https://cdn-icons-png.flaticon.com/512/4140/4140060.png';
 
         return Larafirebase::withTitle($title)
             ->withBody($body)
-            ->withImage('https://firebase.google.com/images/social.png')
+            ->withImage('https://img.freepik.com/vector-gratis/senal-advertencia-triangulo-rojo-ilustracion-arte-vectorial_56104-865.jpg?w=740&t=st=1695986238~exp=1695986838~hmac=e4584b1d466880abfdd0ff55f35ca1150b61a04a48cad577ea888fdf4ef02e8c')
             ->withIcon($avatar)
             ->withSound('notification.ogg')
             ->withClickAction('FLUTTER_NOTIFICATION_CLICK')
