0b7983f510f00b407dfd23505157ef954c7c9ec7
Piero <piero.suarez.zuniga@alumnos.uta.cl>
2023-10-22 17:13:56 -0300

    interfaz desactivada

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
     {{-- CSS--}} 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @viteReactRefresh
-    @vite(['resources/sass/app.scss']) 
+    @vite(['resources/sass/app.scss'])  --}}
     {{-- Scripts --}}
     @stack('head-scripts')
 </head>
@@ -22,8 +22,9 @@
 
     {{-- del esteban --}}
      <!-- Header -->
-    @include('includes.header')
-    @include('includes.menu-hamburguesa')    
+    {{-- @include('includes.header')
+    @include('includes.menu-hamburguesa')     --}}
+    @include('includes.navbar')
     @yield('body')
     <div class="container py-5">
         @yield('content')
