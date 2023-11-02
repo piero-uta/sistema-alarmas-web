9247adeedc0dd7f2abc455a9c3d410d81c6dd8fb
Piero <piero.suarez.zuniga@alumnos.uta.cl>
2023-11-01 22:24:27 -0300

    Revert "Revert "Merge branch 'back-permisos' into mapas""
    This reverts commit 5521002b6164780ba96c6bc0906f8c5fe0850faf.

--- app/Http/Controllers/Direcciones.php
+++ app/Http/Controllers/Direcciones.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Direccion;
+use App\Models\Comunidad;
+use App\Models\Perfil;
+use App\Models\UsuarioComunidad;
+use App\Models\PermisoPerfil;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Permiso;
+
 use Illuminate\Support\Facades\Session;
 use App\Models\Comunidad;
 
@@ -11,14 +18,52 @@ use App\Models\Comunidad;
 class Direcciones extends Controller
 {
     //
+    public function getPermisos($comunidad){
+        $user = Auth::user();
+        $usuarioComunidad = UsuarioComunidad::where('usuario_id',$user->id)
+        ->where('comunidad_id', $comunidad->id)
+        ->get()
+        ->toArray();
+        $total_result = [];
+        foreach ($usuarioComunidad as $uc) {
+            $perfil = Perfil::where('id',$uc["perfil_id"])->first();
+            $permisoPerfil = PermisoPerfil::where('perfil_id', $perfil->id)
+            ->get()
+            ->toArray();
+            $result = [];
+            foreach($permisoPerfil as $pp) {
+                $permiso = Permiso::where('id', $pp["permiso_id"])->first();
+                array_push($result, $permiso->nombre);
+            }
+            $total_result = array_merge($result, $total_result);
+        }
+        return $total_result;
+    }
+
     public function index()
     {
         // obtener comunidad_id en session
         $comunidad_id = Session::get('comunidad_id');
+        $comunidad = Comunidad::find($comunidad_id);
+
         // obtener todas las direcciones de la comunidad
         $direcciones = Direccion::where('comunidad_id', $comunidad_id)->get();
         // retornar la vista direcciones.index
-        return view('direcciones.index', compact('direcciones'));
+
+        $permisos = $this->getPermisos($comunidad);
+
+        if(in_array('DireccionesUsuario-c', $permisos)||
+        in_array('DireccionesUsuario-r', $permisos)||
+        in_array('DireccionesUsuario-u', $permisos)||
+        in_array('DireccionesUsuario-d', $permisos)){
+        return view('direcciones.index', compact('direcciones','permisos'));
+        }else{
+            return view('main-dashboard');
+
+            // return redirect()->route('login');
+        }
+
+
     }
 
     public function formularioGuardar(Request $request)
@@ -28,8 +73,7 @@ class Direcciones extends Controller
         $comunidad = Comunidad::where('id', $comunidad_id)->first();
         $parametros['comunidad'] = $comunidad;
         if($request->has('id')){
-            // verificar comunidad 
-            
+            // verificar comunidad             
             $direccion = Direccion::where('id', $request->id)->where('comunidad_id', $comunidad_id)->first();
             if(!$direccion){
                 //agregar error a parametros
@@ -69,7 +113,7 @@ class Direcciones extends Controller
         $direccion->numero = $request->numero;
         $direccion->representante = $request->representante;
         $direccion->comunidad_id = $comunidad_id;
-        
+
         $direccion->codigo = $request->codigo;
         $direccion->piso = $request->piso;
         $direccion->latitud = $request->latitud;
@@ -82,7 +126,7 @@ class Direcciones extends Controller
         }else{
             $direccion->activo = 0;
         }
-        
+
         $direccion->save();
         return redirect()->route('direcciones.index')->with(['mensaje' => 'Direccion guardada correctamente']);
     }
